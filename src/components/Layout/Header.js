227e9d5a4b6d0501f590ac5ead15ee4fd9358c67
Arnab Jyoti Borah <113490566+Arnab7747@users.noreply.github.com>
2024-02-16 13:39:06 +0530

    header

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import classes from './Header.module.css'
+const imageUrl = 'https://img.freepik.com/free-photo/handrawn-barbecue-elements_125540-588.jpg?w=900&t=st=1708069945~exp=1708070545~hmac=8a927847450366695a702c8a2669639f0227301e81ea0c16a4ee683822f58fb7';
+
+export default function Header() {
+  return (
+    <div>
+         <React.Fragment>
+            <header className={classes.header}>
+                <h1>Quick Meal</h1>
+                <button>Cart</button>
+
+            </header>
+            <div className={classes.mainImage}>
+                <img src={imageUrl} alt='img'/>
+
+            </div>
+         </React.Fragment>
+      
+    </div>
+  )
+}

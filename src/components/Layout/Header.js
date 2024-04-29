34921d280e8d155dc0ab57c84ad77e27bddd198f
Arnab Jyoti Borah <113490566+Arnab7747@users.noreply.github.com>
2024-04-29 09:43:41 +0530

    functioning done

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,23 +1,23 @@
-import React from 'react'
+import { Fragment } from 'react';
+
 import HeaderCartButton from './HeaderCartButton';
-import classes from './Header.module.css'
-const imageUrl = 'https://img.freepik.com/free-photo/handrawn-barbecue-elements_125540-588.jpg?w=900&t=st=1708069945~exp=1708070545~hmac=8a927847450366695a702c8a2669639f0227301e81ea0c16a4ee683822f58fb7';
 
-export default function Header() {
+import classes from './Header.module.css';
+
+const mealsImage = URL
+
+const Header = (props) => {
   return (
-    <div>
-         <React.Fragment>
-            <header className={classes.header}>
-                <h1>Quick Meal</h1>
-                <HeaderCartButton />
+    <Fragment>
+      <header className={classes.header}>
+        <h1>ReactMeals</h1>
+        <HeaderCartButton onClick={props.onShowCart} />
+      </header>
+      <div className={classes['main-image']}>
+        <img src={mealsImage} alt='A table full of delicious food!' />
+      </div>
+    </Fragment>
+  );
+};
 
-            </header>
-            <div className={classes.mainImage}>
-                <img src={imageUrl} alt='img'/>
-
-            </div>
-         </React.Fragment>
-      
-    </div>
-  )
-}
+export default Header;
--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import { Fragment } from 'react'
-import MealsSummary from './MelasSummary'
-import AvailableMeals from './AvailableMeals'
+import { Fragment } from 'react';
 
+import MealsSummary from './MealsSummary';
+import AvailableMeals from './AvailableMeals';
 
-export default function Meals() {
+const Meals = () => {
   return (
     <Fragment>
-        <MealsSummary/>
-        <AvailableMeals/>
-      
+      <MealsSummary />
+      <AvailableMeals />
     </Fragment>
-  )
-}
+  );
+};
+
+export default Meals;
